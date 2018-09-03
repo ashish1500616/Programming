48bc59a5dee9a9e21cf3ab55f6d257b14bcb0d61
ashish verma <ashish1500616@gmail.com>
2018-09-03 20:14:20 +0530

    Fix Error in Preorder Traversal Non Recurrsive

--- ALGO/python/Tree/PreorderTraversal/non_recusive_preorder.py
+++ ALGO/python/Tree/PreorderTraversal/non_recusive_preorder.py
@@ -8,10 +8,10 @@ def non_recursive_preorder(root, result):
     while stack:
         node = stack.pop()
         result.append(root.data)
-        if node.left:
-            stack.append(node.left)
         if node.right:
             stack.append(node.right)
+        if node.left:
+            stack.append(node.left)
 
     # Time Complexity = O(n)
     # Space Complexity =O(n)
