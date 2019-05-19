e37ca7d71042b61f6aa6aa06546a023a66d92ae9
Ashish Verma <ashish1500616@gmail.com>
2019-05-19 16:35:10 +0530

    Range Minimum Query

--- ALGO/DataStructure/SegmentTree/RangeMinQuery.cpp
+++ ALGO/DataStructure/SegmentTree/RangeMinQuery.cpp
@@ -1,6 +1,20 @@
 // Range Minimum Query
 // https://www.hackerearth.com/practice/data-structures/advanced-data-structures/segment-trees/tutorial/
 
+
+// SAMPLE INPUT 
+// 5 5
+// 1 5 2 4 3
+// q 1 5
+// q 1 3
+// q 3 5
+// u 3 6
+// q 1 5
+// SAMPLE OUTPUT 
+// 1
+// 1
+// 2
+// 1
 #include <bits/stdc++.h>
 using namespace std;
 
