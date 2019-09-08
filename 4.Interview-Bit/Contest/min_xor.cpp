684bb66a45a2d2bd298e460e7222487cd7c13b5b
Ashish Verma <ashish1500616@gmail.com>
2019-09-08 12:44:23 +0530

    Hashing
    1.  Longest consecutive Sequence .

---
+++ 4.Interview-Bit/Contest/min_xor.cpp
@@ -0,0 +1,70 @@
+int powExpo(int a,int b)
+{
+    if(b==0)
+    return 1;
+    int ans=powExpo(a,b/2);
+    ans*=ans;
+    if(b&1)
+    ans*=a;
+    return ans;
+}
+
+int ver(vector<int> r)
+{
+    int x = 0;
+    for (int i = 0; i < 40; i++)
+    {
+        if (r[i]==1)
+        {
+            x += powExpo(2, i);
+        }
+    }
+    return x;
+}
+int Solution::solve(int A, int B)
+{
+    vector<int> hash(40, 0);
+    int i = 0;
+    int s = 0;
+    int x = A;
+    while (A)
+    {
+        if (A & 1)
+        {
+            hash[i] = 1;
+            s++;
+        }
+        A >>= 1;
+        i++;
+    }
+    if (s == B)
+        return x;
+    vector<int> r;
+    r = hash;
+    if (s < B)
+    {
+        int d = B - s;
+        for (i = 0; i < 40; i++)
+        {
+            if (r[i] == 0 && d)
+            {
+                r[i] = 1;
+                d--;
+            }
+        }
+        return ver(r);
+    }
+    else
+    {
+        int d = s - B;
+        for (int j = 0; j < 40; j++)
+        {
+            if (r[j] == 1 && d)
+            {
+                r[j] = 0;
+                d--;
+            }
+        }
+        return ver(r);
+    }
+}
--- 4.Interview-Bit/Hashing/longest_consecutive_seq.cpp
+++ 4.Interview-Bit/Hashing/longest_consecutive_seq.cpp
@@ -1,69 +1,67 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+// N log N solution
+int longestConsecutive(const vector<int> &A)
+{
 
-int longestConsecutive(const vector<int> &A) {
-
-    int l=A.size();
+    int l = A.size();
     vector<int> a(A);
-    if(l==1)
-    return l;
-    // int mx =*max_element(a.begin(),a.end());
-    // if(mx<0)
-    // mx=mx*-1;
-    // vector<int> h(mx,0);
-    // for (auto i=0;i<a.size();i++)
-    // {
-    //     if(a[i]<0)
-    //     a[i]=a[i]*-1;
-    //     if(h[a[i]]==0)
-    //     h[a[i]]+=1;
-    // }
- 
-    // int c=0,m=INT_MIN;
-    // for(auto x=1;x<h.size();x++)
-    // {
-    //     if(x==1)
-    //     {
-    //         c+=1;
-    //     }
-    //     else
-    //     c=0;
-    //     m=max(m,c);
-    // }
-    // return m;
-    
-    map <int,int> m;
-    for(auto x:A)
+    if (l == 1)
+        return l;
+    map<int, int> m;
+    for (auto x : A)
     {
-        m[x]=1;
+        m[x] = 1;
     }
-    int c=1;
-    auto m_t=m.begin();
-    int ma=max(m_t->first,INT_MIN);
-    for(auto i=(++m.begin());i!=m.end();i++)
+    int c = 1;
+    auto m_t = m.begin();
+    int ma = max(m_t->first, INT_MIN);
+    for (auto i = (++m.begin()); i != m.end(); i++)
     {
-        cout<<"mt"<<m_t->first<<"\n";
-        cout<<i->first<<"\n";
-        if((i->first)-(m_t->first)==1)
+        cout << "mt" << m_t->first << "\n";
+        cout << i->first << "\n";
+        if ((i->first) - (m_t->first) == 1)
         {
-            c+=1;
-            ma=max(c,ma);
-            cout<<"count"<<c<<"\n";
+            c += 1;
+            ma = max(c, ma);
+            cout << "count" << c << "\n";
         }
         else
         {
-            c=1;
-            cout<<"count"<<c<<"\n";
-
+            c = 1;
+            cout << "count" << c << "\n";
         }
         m_t++;
     }
     return ma;
 }
 
+// O( N )  Solution
+int Solution::longestConsecutive(const vector<int> &A)
+{
+    unordered_set<int> s;
+    for (auto i : A)
+    {
+        s.insert(i);
+    }
+    int c_s = 0;
+    int m_c = INT_MIN;
+    for (auto i : s)
+    {
+        if (s.find(i - 1) == s.end())
+        {
+            while (s.find(i++) != s.end())
+                c_s++;
+        }
+        m_c = max(m_c, c_s);
+        c_s = 0;
+    }
+    return m_c;
+}
+
 int main()
 {
-    vector<int> A={ 97, 86, 67, 19, 107, 9, 8, 49, 23, 46, -4, 22, 72, 4, 57, 111, 20, 52, 99, 2, 113, 81, 7, 5, 21, 0, 47, 54, 76, 117, -2, 102, 34, 12, 103, 69, 36, 51, 105, -3, 33, 91, 37, 11, 48, 106, 109, 45, 58, 77, 104, 60, 75, 90, 3, 62, 16, 119, 85, 63, 87, 43, 74, 13, 95, 94, 56, 28, 55, 66, 92, 79, 27, 42, 70 };
-    cout<<longestConsecutive(A)<<"\n";
+    vector<int> A = {97, 86, 67, 19, 107, 9, 8, 49, 23, 46, -4, 22, 72, 4, 57, 111, 20, 52, 99, 2, 113, 81, 7, 5, 21, 0, 47, 54, 76, 117, -2, 102, 34, 12, 103, 69, 36, 51, 105, -3, 33, 91, 37, 11, 48, 106, 109, 45, 58, 77, 104, 60, 75, 90, 3, 62, 16, 119, 85, 63, 87, 43, 74, 13, 95, 94, 56, 28, 55, 66, 92, 79, 27, 42, 70};
+    cout << longestConsecutive(A) << "\n";
     return 0;
 }
