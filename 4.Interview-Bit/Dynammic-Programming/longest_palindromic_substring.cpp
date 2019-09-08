ff9420e5aa18348831fac83bf7ed4d5c016326ae
Ashish Verma <ashish1500616@gmail.com>
2019-09-08 21:47:00 +0530

    Dynammic Programming
    1. Longest Palindromic substring -
       SC-  O(1)

--- 4.Interview-Bit/Dynammic-Programming/longest_palindromic_substring.cpp
+++ 4.Interview-Bit/Dynammic-Programming/longest_palindromic_substring.cpp
@@ -1,7 +1,9 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Dynammic Approach Exceeds Memory limit on Interviewbit
+// Time complexity: O ( n^2 )
+// Auxiliary Space: O ( n^2 )
 void longestPalindrome(string A)
 {
     int n = A.size();
@@ -37,9 +39,9 @@ void longestPalindrome(string A)
     {
         for (int j = A.size() - 1; j >= i; j--)
         {
-             if (M[i][j] == 1)
+            if (M[i][j] == 1)
             {
-            //    cout<<i << " " <<j <<" \n";
+                //    cout<<i << " " <<j <<" \n";
                 if ((j - i + 1) > m)
                 {
                     m = j - i + 1;
@@ -50,10 +52,49 @@ void longestPalindrome(string A)
         }
     }
     // cout<<ai<<" "<<bi<<"\n";
-    cout<< A.substr(ai,bi-ai+1);
+    cout << A.substr(ai, bi - ai + 1);
 }
+
+// T c - O (N^2)
+// S C - O (1)
+
+void longestPalindrome(string A)
+{
+    int n = A.size();
+    int ml = 1;
+    int s, e;
+    for (int i = 1; i < n; i++)
+    {
+        int l = i - 1;
+        int h = i + 1;
+        while (l >= 0 && h < n && A[l] == A[h])
+        {
+            if (h - l + 1 > ml)
+            {
+                s = l;
+                ml = h - l + 1;
+            }
+            --l;
+            h++;
+        }
+        l = i - 1;
+        h = i;
+        while (l >= 0 && h < n && A[l] == A[h])
+        {
+            if (h - l + 1 > ml)
+            {
+                s = l;
+                ml = h - l + 1;
+            }
+            --l;
+            h++;
+        }
+    }
+    cout << A.substr(s, ml);
+}
+
 int main()
 {
-    longestPalindrome("abbcccbbbcaaccbababcbcabca");
+    longestPalindrome("");
     return 0;
 }
