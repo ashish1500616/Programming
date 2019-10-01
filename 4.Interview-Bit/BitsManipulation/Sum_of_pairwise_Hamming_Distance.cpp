ef24b6c01f894aae3fdfd455f678dcba3ee771d2
Ashish Verma <ashish1500616@gmail.com>
2019-10-01 17:24:27 +0530

    Bits-Manipulation
    -Different Bits Sum Pairwise.
    -Sum Of Pairwise hamming Distance.

---
+++ 4.Interview-Bit/BitsManipulation/Sum_of_pairwise_Hamming_Distance.cpp
@@ -0,0 +1,24 @@
+using ll = long long;
+#define mod 1000000007
+#define w(n) cout << #n << "\tis\t" << n << "\n"
+int Solution::hammingDistance(const vector<int> &A)
+{
+    ll sum = 0;
+    int n = A.size();
+    // traverse over all bits
+    for (int i = 0; i < 32; i++)
+    {
+        // count number of elements with i'th bit set
+        int count = 0;
+        for (int j = 0; j < n; j++)
+            if ((A[j] & (1 << i)))
+                count++;
+
+        // Add "count * (n - count) * 2" to the answer
+        sum += (count * 1ll * (n - count) * 2);
+        sum %= mod;
+    }
+
+    // return ans;
+    return sum;
+}
