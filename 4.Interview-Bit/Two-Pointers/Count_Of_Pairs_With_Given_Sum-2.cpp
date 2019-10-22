40d8a409c5e5dea1566e2175e633f5c07aecf66c
Ashish Verma <ashish1500616@gmail.com>
2019-10-22 11:37:47 +0530

    Two Pointers.
    - Count Of Pairs With Given Sum.

---
+++ 4.Interview-Bit/Two-Pointers/Count_Of_Pairs_With_Given_Sum-2.cpp
@@ -0,0 +1,35 @@
+int Solution::solve(vector<int> &A, int B)
+{
+    int i = 0, j = A.size() - 1;
+    long long ans = 0;
+    while (i < j)
+    {
+        if (A[i] + A[j] > B)
+            j--;
+        else if (A[i] + A[j] < B)
+            i++;
+        else
+        {
+            int x = A[i], cnt1 = 0;
+            while (A[i] == x && i < A.size())
+            {
+                i++, cnt1++;
+            }
+            int y = A[j], cnt2 = 0;
+            while (A[j] == y && j >= 0)
+            {
+                j--, cnt2++;
+            }
+            if (x == y)
+            {
+                ans += ((cnt1)*1LL * (cnt1 - 1) / 2);
+            }
+            else if (cnt1 > 0 && cnt2 > 0)
+                ans += cnt1 * 1LL * cnt2;
+            else
+                ans += 1;
+        }
+        ans %= 1000000007;
+    }
+    return ans;
+}
