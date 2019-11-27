ffbb5e5ba76e46074309772cedac0b75f62fa98a
Ashish <ashish1500616@gmail.com>
2019-11-27 23:08:52 +0530

    Quick Sort Partition Implementation

--- 1.Algorithms/C++/Sorting/quick_sort.cpp
+++ 1.Algorithms/C++/Sorting/quick_sort.cpp
@@ -16,12 +16,34 @@ int partition(vector<int> &A, int l, int r)
     swap(A[++i], A[pivot]);
     return i;
 }
-
+//  Vivek Sir Partition Using Two Pointers. 
+int partiton_pointers(vector<int> &A,int l,int r)
+{
+    int p=l;// Taking Pivot the first element .
+    ++l;
+    while(l<r)
+    {
+        while(l<r && A[l]<=A[p])
+        {
+            ++l;
+        }
+        while(r>p && A[r]>A[p])
+        {
+            --r;
+        }
+        if(l<r)
+        {
+            swap(A[l],A[r]);
+        }
+    }
+    swap(A[p],A[r]);
+    return r;
+}
 void quickSort(vector<int> &A, int l, int h)
 {
     if (l < h)
     {
-        int p = partition(A, l, h);
+        int p = partiton_pointers(A, l, h);
         quickSort(A, l, p - 1);
         quickSort(A, p + 1, h);
     }
